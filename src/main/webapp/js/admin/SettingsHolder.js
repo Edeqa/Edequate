60d4cad8cdeeae43a6a1a8e55bc65018ec93895c
Tujger <tujger@gmail.com>
2018-04-05 01:14:45 -0400

    EQ-25 SettingsHolder

---
+++ src/main/webapp/js/admin/SettingsHolder.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) Edeqa <http://www.edeqa.com>
+ *
+ * Created 4/5/18.
+ */
+function SettingsHolder(main) {
+    var self = this;
+    var u = main.edequate;
+
+    this.category = DRAWER.SECTION_RESOURCES;
+    this.type = "settings";
+    this.title = "Settings";
+    this.menu = "Settings";
+    this.icon = "settings";
+    this.priority = -10;
+
+    var div;
+    var dialogConfirm;
+
+    this.start = function() {
+        div = main.content;
+    };
+
+    this.resume = function() {
+        u.clear(div);
+        var titleNode = u.create(HTML.H2, "Settings", div);
+
+    }
+}
